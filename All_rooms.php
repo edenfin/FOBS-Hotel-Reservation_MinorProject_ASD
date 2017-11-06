cf8eb8ba3d810e816ab37975029a43ebd7f87366
edenfin <32209435+edenfin@users.noreply.github.com>
2017-11-06 17:18:21 +0800

    page for all rooms, including details

---
+++ All_rooms.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="shortcut icon" href="images/hotel_rooms/logo.jpg">
+<link rel="stylesheet" type="text/css" href="css.css">
+<title> FOBS | Rooms & Suite</title>
+</head>
+
+<body>
+<body bgcolor="#A7C8D1">
+<!MARGINS--------------------------------------------------------------------------------->
+<div id="left_margin">
+<br><br><br><br><br><br><br><br><br><br><br>
+<table border="0" width="50px">
+<tr>
+<td><div class="">
+<a href="http://www.google.com" target="_blank">
+<img src="images/social media/google.jpg" style width="50px" height="50px"></a>
+</div></td></tr>
+
+<tr>
+<td><div class="">
+<a href="http://www.facebook.com" target="_blank">
+<img src="images/social media/facebook.png" style width="50px" height="50px"></a>
+</div></td></tr>
+
+<tr>
+<td><div class="">
+<a href="http://www.twitter.com" target="_blank">
+<img src="images/social media/Twitter.png" style width="50px" height="50px"></a>
+</div></td></tr>
+
+<tr>    
+<td><div class="">
+<a href="http://www.instagram.com" target="_blank">
+<img src="images/social media/instagram.jpg" style width="50px" height="50px"></a>
+</div></td></tr>
+
+<tr>    
+<td><div class="" target="_blank">
+<a href="http://www.youtube.com" target="_blank">
+<img src="images/social media/youtube.png" style width="50px" height="50px"></a>
+</div></td></tr>
+</table>
+</div>
+
+<div id="right_margin">
+
+</div>
+
+<!TOP LINKS--------------------------------------------------------------------------------->
+<div id="top_links">
+<table border="0" width="500px">
+<tr>
+<td><center><h1>
+<a href="http://localhost/Hotel%20Reservation/Home.php">HOME</a></h1>
+</center></td>
+
+<td><center><h1>
+<a href="http://localhost/hotel%20reservation/Rooms.php">ROOMS</a></h1>
+</center></td>
+
+<td><h1>
+<a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/trivias.html">CONTACT USE</a></h1>
+</td>
+
+<td><h1>
+    <a href="http://localhost/Hotel%20Reservation/Signup.php">Sign Up</a></h1>
+</td>    
+    
+<td><h1>
+    <a href="http://localhost/Hotel%20Reservation/Login.php">Login</a></h1>
+</td>  
+
+</tr>
+</table>
+</div>
+
+<!HEADER--------------------------------------------------------------------------------->
+<div id="header">
+<table border="0" width="900px">
+<tr>
+<td><h2> FOBS - Hotel Reservation </h2></td>
+
+<td>
+<img src="images/hotel_rooms/logo.jpg" style width="200px" height="95px">
+</td>
+
+</tr>
+</table>
+</div>
+
+<!PLACES LINKS--------------------------------------------------------------------------------->
+
+<div id="link_places">
+<!--
+<ul id="menu">
+    <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/church.html">Churches</a></li>
+    <li><a href="">Stores</a>
+        <ul>
+            <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/malls.html">Malls</a></li>
+            <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/submenu%20files/aldevinco.html">Honorable Mention: Aldevinco Shopping Center</a></li>
+        </ul>
+    </li>
+    <li><a href="">Food</a>
+        <ul>
+            <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/restaurants.html">Restaurants</a></li>
+            <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/submenu%20files/locals choice.html">Local's Choice</a></li>
+        </ul>
+    </li>
+    <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/parks.html">Parks</a>
+    <li><a href="">Nightlife</a>
+        <ul>
+            <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/Resto-Bars.html">Resto-Bar</a></li>
+            <li><a href="">Rated 18+</a></li>
+            <li><a href="">Fun</a></li>
+        </ul>
+    </li>
+</ul>
+-->
+    <center />
+    <h10>Rooms & Suite Types </h10>
+</div>
+
+<!MARQUEE LINKS--------------------------------------------------------------------------------->
+
+<div id="marquee_links">
+<table border="0" width="950px" height="65px">
+<tr>
+<td width="160px"><h4>Recommended Rooms</h4></td>
+
+<td>
+<marquee behavior="scroll" direction="left" scrollamount="5" 
+onmouseover="this.stop ();" onmouseout="this.start ();">
+
+<h5>Room 1</h5>
+<a href="">
+<img class="thumbnail" src="images/hotel_rooms/1.jpg" style width="50px" height="50px"></a>
+
+<h5>Room 2</h5>
+<img class="thumbnail" src="images/hotel_rooms/2.jpg" style width="50px" height="50px">
+
+<h5>Room 3</h5>
+<img class="thumbnail" src="images/hotel_rooms/3.jpg" style width="50px" height="50px">
+
+<h5>Room 4</h5>
+<img class="thumbnail" src="images/hotel_rooms/4.jpg" style width="50px" height="50px">
+
+<h5>Room 5</h5>
+<img class="thumbnail" src="images/hotel_rooms/5.jpg" style width="50px" height="50px">
+
+</marquee></td>
+</tr>
+</table>
+</div>
+
+<!BODY---------------------------------------------------------------------------------------------->
+<div id="body">
+<br><br><br>
+<center />
+<h6> Single Rooms </h6>
+<br><br><br>
+<h7>  "All single rooms here will definetely satisfy you" </h7>
+
+<br><br><br><br>
+<center>
+<table border="0" width="800px" height="700px">
+<tr>
+<td>
+<div class="grow pic">
+    <div class="tooltip">
+<img src="images/hotel_rooms/single_rooms/1.jpg">
+    <span class="tooltiptext">hey!</span>
+    </div>
+<h8> FOBS | Single Room (A) </h8></div>
+</td>
+<td><div class="grow pic">
+<img src="images/hotel_rooms/single_rooms/2.jpg">
+<br>
+<h8> FOBS | Single Room (B) </h8></div>
+</td>
+
+
+<tr>
+<td><div class="grow pic">
+<img src="images/hotel_rooms/single_rooms/3.jpg">
+<br>
+<h8> FOBS | Single Room (C) </h8></div>
+</td>
+<td><div class="grow pic">
+<img src="images/hotel_rooms/single_rooms/4.jpg">
+<br>
+<h8> FOBS | Single Room (D) </h8></div>
+</td>
+</tr>
+    
+<tr>
+<td>
+<div class="grow pic">
+<img src="images/hotel_rooms/single_rooms/5.jpg">
+    <br>
+<h8> FOBS | Single Room (E) </h8></div>
+</td>
+</tr>    
+</table></center>
+</div>
+
+<!FOOTER---------------------------------------------------------------------------------------------->
+<div id="footer">
+<center>
+<h8> FOBS - Hotel Reservation --- All Rights Reserved :)) </h8>
+</center>
+</div>
+</body>
+</body>
+</html>
