b1a3676df145bcc9cdb8d46ddc3970f01c31b754
edenfin <32209435+edenfin@users.noreply.github.com>
2017-11-06 17:00:30 +0800

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="shortcut icon" href="images/hotel_rooms/logo.jpg">
+<title> FOBS | Home </title>
+<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+
+<body>
+
+<!MARGINS--------------------------------------------------------------------------------->
+<div id="left_margin">
+<br><br><br><br><br><br><br><br><br><br><br>
+<table border="0" width="50px">
+<tr>
+<td><div class="">
+<a href="http://www.google.com" target="_blank">
+<img src="images/social media/google.jpg" style width="50px" height="50px"></a>
+</div></td></tr>
+
+<tr>
+<td><div class="">
+<a href="http://www.facebook.com" target="_blank">
+<img src="images/social media/facebook.png" style width="50px" height="50px"></a>
+</div></td></tr>
+
+<tr>
+<td><div class="">
+<a href="http://www.twitter.com" target="_blank">
+<img src="images/social media/Twitter.png" style width="50px" height="50px"></a>
+</div></td></tr>
+
+<tr>    
+<td><div class="">
+<a href="http://www.instagram.com" target="_blank">
+<img src="images/social media/instagram.jpg" style width="50px" height="50px"></a>
+</div></td></tr>
+
+<tr>    
+<td><div class="" target="_blank">
+<a href="http://www.youtube.com" target="_blank">
+<img src="images/social media/youtube.png" style width="50px" height="50px"></a>
+</div></td></tr>
+</table>
+</div>
+
+<div id="right_margin">
+
+</div>
+
+<!TOP LINKS--------------------------------------------------------------------------------->
+<div id="top_links">
+<table border="0" width="500px">
+<tr>
+<td><center><h1>
+<a href="http://localhost/Hotel%20Reservation/Home.php"> 
+ HOME</a> </h1> 
+</center></td>
+
+<td><center><h1>
+    
+    <a href="http://localhost/hotel%20reservation/Rooms.php">ROOMS</a></h1>
+</center></td>
+
+<td><h1>
+    <a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/trivias.html">CONTACT US</a></h1>
+</td>
+
+<td><h1>
+    <a href="http://localhost/Hotel%20Reservation/Signup.php">Sign Up</a></h1>
+</td>    
+    
+<td><h1>
+    <a href="http://localhost/Hotel%20Reservation/Login.php">Login</a></h1>
+</td>    
+    
+</tr>
+</table>
+</div>
+
+<!HEADER--------------------------------------------------------------------------------->
+<div id="header">
+<table border="0" width="900px">
+<tr>
+<td><h2> FOBS - Hotel Reservation </h2></td>
+
+<td>
+<img src="images/hotel_rooms/logo.jpg" style width="200px" height="90px">
+</td>
+
+</tr>
+</table>
+</div>
+
+<!PLACES LINKS--------------------------------------------------------------------------------->
+<div id="link_places">
+    <!--
+<ul id="menu">
+    <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/church.html">Churches</a></li>
+    <li><a href="">Stores</a>
+        <ul>
+            <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/malls.html">Malls</a></li>
+            <li><a href="#">Honorable Mention: Aldevinco Shopping Center</a></li>
+        </ul>
+    </li>
+    <li><a href="#">Food</a>
+        <ul>
+            <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/restaurants.html">Restaurants</a></li>
+            <li><a href="#">Local's Choice</a></li>
+        </ul>
+    </li>
+    <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/parks.html">Parks</a>
+    <li><a href="">Nightlife</a>
+        <ul>
+            <li><a href="file:///D:/Ghost/Get%20to%20work!/Latest%20HTML/Link%20Places/Resto-Bars.html">Resto-Bar</a></li>
+            <li><a href="">Rated 18+</a></li>
+            <li><a href="">Fun</a></li>
+        </ul>
+    </li>
+</ul>
+-->
+    <center />
+    <h10>Home</h10>
+</div>
+<!MARQUEE LINKS--------------------------------------------------------------------------------->
+
+<div id="marquee_links">
+<table border="0" width="950px" height="65px">
+<tr>
+<td width="160px"><h4>Recommended Rooms</h4></td>
+
+<td>
+<marquee behavior="scroll" direction="left" scrollamount="5" 
+onmouseover="this.stop ();" onmouseout="this.start ();">
+
+<h5>Room 1</h5>
+<a href="">
+<img class="thumbnail" src="images/hotel_rooms/1.jpg" style width="50px" height="50px"></a>
+
+<h5>Room 2</h5>
+<img class="thumbnail" src="images/hotel_rooms/2.jpg" style width="50px" height="50px">
+
+<h5>Room 3</h5>
+<img class="thumbnail" src="images/hotel_rooms/3.jpg" style width="50px" height="50px">
+
+<h5>Room 4</h5>
+<img class="thumbnail" src="images/hotel_rooms/4.jpg" style width="50px" height="50px">
+
+<h5>Room 5</h5>
+<img class="thumbnail" src="images/hotel_rooms/5.jpg" style width="50px" height="50px">
+
+</marquee></td>
+</tr>
+</table>
+</div>
+
+<!BODY---------------------------------------------------------------------------------------------->
+<div id="gallery_container">
+<div class="container">
+<img class='photo'  src="images/hotel_rooms/1.jpg">
+<img class='photo'  src="images/hotel_rooms/2.jpg">
+<img class='photo'  src="images/hotel_rooms/3.jpg">
+<img class='photo'  src="images/hotel_rooms/4.jpg">
+<img class='photo'  src="images/hotel_rooms/5.jpg">
+<img class='photo'  src="images/hotel_rooms/6.jpg">
+<img class='photo'  src="images/hotel_rooms/7.jpg">
+<img class='photo'  src="images/hotel_rooms/8.jpg">
+</div>
+</div>
+
+<div id="body">
+<!--
+<pre>
+<h9>Davao City</h9>
+<br><br>
+<h6>
+	"Davao City is a city on Mindanao, Philippines. As of the 2010 census, it had a population 
+of 1,449,296 people, making it the fourth-most-populous city in the Philippines and the most 
+populous in Mindanao. It is the center of Metro Davao, the third most populous metropolitan 
+area in the Philippines (as of 2010 Census with a population of 2.26 million, after Metro 
+Manila's 11.86 million and Metro Cebu's 2.55 million). With a total land area of 2,444 square 
+kilometers, the city is the largest in the country in terms of land area. The city serves as 
+the main trade, commerce, and industry hub of Mindanao and the regional center of Davao 
+Region. Davao is home to Mount Apo, the highest mountain in the Philippines. Davao is also 
+known as the "Durian Capital" of the Philippines." 
+<h7><a href="http://en.wikipedia.org/wiki/Davao_City">(source:http://en.wikipedia.org/wiki/Davao_City)</a></h7>
+
+	Every person that visits Mindanao here in the Philippines needs to know what Davao 
+City can offer their hungry eyes for places that fills with curiosity. This website is dedicated 
+for tourists that are or will visit Davao city. In addtion, even our locals here in Davao are 
+welcome on this site. Feel free to browse.
+</h6>
+</pre>
+-->
+</div>
+
+<!FOOTER---------------------------------------------------------------------------------------------->
+<div id="footer">
+<center>
+<h8> FOBS Hotel Reservation --- All Rights Reserved :)) </h8>
+</center>
+</div>
+</body>
+</html>
